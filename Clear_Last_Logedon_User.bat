e80a41c2760009bcb6b1294e62563adc6a89ad0d
DoktorIgnoranz <84376132+DoktorIgnoranz@users.noreply.github.com>
2023-01-06 11:46:30 +0100

    Update Clear_Last_Logedon_User.bat

--- Clear_Last_Logedon_User.bat
+++ Clear_Last_Logedon_User.bat
@@ -1,6 +1,8 @@
 @echo off
 
-reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnDisplayName
-reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnSAMUser
-reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnUser
-reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnUserSID
+reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnDisplayName /f
+reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnSAMUser /f
+reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnUser /f
+reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnUserSID /f
+
+shutdown -s -t 15
