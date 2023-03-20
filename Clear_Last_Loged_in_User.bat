ed81cf32c10d501e26ad181ca2284230cd35adb2
DoktorIgnoranz <84376132+DoktorIgnoranz@users.noreply.github.com>
2023-03-20 12:17:02 +0100

    Update Clear_Last_Loged_in_User.bat
    Added a Admin permision check

--- Clear_Last_Loged_in_User.bat
+++ Clear_Last_Loged_in_User.bat
@@ -1,8 +1,27 @@
 @echo off
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed.
+    ) else (
+        echo Failure: Current permissions inadequate.
+	pause >nul 
+	 echo Press any Key to exit
+	exit /b 0
+    )
+    	echo Press Enter to Continue
+    pause >nul
+
+
+@echo off
 
 reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnDisplayName /f
 reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnSAMUser /f
 reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnUser /f
 reg delete  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\ /v LastLoggedOnUserSID /f
 
-shutdown -s -t 15
+shutdown -L
